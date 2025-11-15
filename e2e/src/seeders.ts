d89ce380ffc2265ffd246df4744970b7d562f0c7
Noel De Martin <noel@noeldemartin.com>
2025-11-15 21:09:54 +0100

    Fix E2E tests

--- e2e/src/seeders.ts
+++ e2e/src/seeders.ts
@@ -1,3 +1,4 @@
+import { after } from '@noeldemartin/utils';
 import type { Page } from '@playwright/test';
 
 export async function createShow(
@@ -11,4 +12,7 @@ export async function createShow(
         // oxlint-disable-next-line no-unsafe-argument
         root.shows.$jazz.push(globalThis.$e2e.jazzSchemas.Show.create(attributes));
     }, attributes as any); // oxlint-disable-line no-explicit-any
+
+    // Some browsers (webkit) need some delays to make sure that the show has been created.
+    await after({ ms: 100 });
 }
